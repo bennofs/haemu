ad92f1fbc2ebe6d89e0ecada3cc5ce361d7d3458
chaosmasttter <fe.kunzmann@gmail.com>
2013-05-14 15:37:52 +0200

    Added checkCondition to Execution.hs

--- src/Haemu/Execution.hs
+++ src/Haemu/Execution.hs
@@ -5,23 +5,32 @@ import Haemu.Monad
 import Haemu.Unit
 import Haemu.Instruction
 import Haemu.Exception
+import Haemu.Types
 import Control.Lens
 import Data.Monoid
 import Control.Monad.Exception
 import qualified Data.Vector as V
 
-data ExecConfig l m r v = ExecConfig
-  { _units :: V.Vector (Unit l m r v)
+data ExecConfig l m = ExecConfig
+  { _units :: V.Vector (Unit l m)
   }
 makeLenses ''ExecConfig
 
 -- | We can combine multiple configurations into one single configuration.
-instance Monoid (ExecConfig l m r v) where
+instance Monoid (ExecConfig l m) where
   mempty = ExecConfig V.empty
   mappend a = units <>~ a ^. units -- ^ This gives the second operand preference.
 
-executeInstruction :: (Monad m, Throws UnknownOptype l)
-                   => ExecConfig l m r v -> Instruction -> EMT l (HaemuM m r v) ()
-executeInstruction c i = do
-  u <- c ^? units . ix (i ^. optype . int) ?-> UnknownOptype (i ^. optype)
-  runInstruction u i
+executeInstruction :: (Monad m, Throws UnknownOptype l) 
+                       => ExecConfig l m -> HaemuState v -> Instruction -> EMT l (HaemuM m) ()
+executeInstruction c s i = do
+  if checkCondition (i ^. condition) (s ^. flags)
+    then do
+      u <- c ^? units . ix (i ^. optype . int) ?-> UnknownOptype (i ^. optype)
+      runInstruction u i
+	else -- TODO
+
+checkCondition :: Condition -> Flags -> Boolean
+checkCondition 0 _ = True
+--TODO
+checkCondition 256 _ = False
