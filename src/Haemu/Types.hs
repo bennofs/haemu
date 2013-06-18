5f3ae2513af0da83e768d4d551ee7ac9a8884e58
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2013-06-18 17:21:29 +0200

    OddWord was fixed, so remove own Word12

--- src/Haemu/Types.hs
+++ src/Haemu/Types.hs
@@ -12,12 +12,8 @@ module Haemu.Types
   ) where
 
 import Data.Word
-import Data.Word.Odd (Word4, OddWord, One, Zero)
+import Data.Word.Odd
 import qualified Data.Vector.Unboxed as V
-
--- | The Word12 type synonym in the OddWord package is wrong. I sent a patch, but until
--- it gets applied and uploaded to hackage we have to use this as a workaround.
-type Word12 = OddWord Word16 (One (One (Zero (Zero ())))) -- 12 = 1100 in binary
 
 type Optype = Word4
 type Opcode = Word12
